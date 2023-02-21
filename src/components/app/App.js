0c5dd2c0289520e32ad719fc78d03e97f5d8f21a
m1arsen <m1arsen.web@gmail.com>
2023-02-21 14:03:06 +0300

    add component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,9 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from '../randomChar/RandomChar';
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import AppBanner from "../appBanner/AppBanner";
+
+import decoration from '../../resources/vision.svg';
 
 function App() {
   return (
@@ -13,6 +16,7 @@ function App() {
           <CharList/>
           <CharInfo/>
         </div>
+        <img className="bg-decoration" src={decoration} alt="vision"/>
       </main>
     </div>
   );
