25245b154a00254f24bc8ac540e78c5a1c5bb6b5
m1arsen <m1arsen.web@gmail.com>
2023-02-21 16:56:40 +0300

    add services

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 
+import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
