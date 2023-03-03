41772c7cfb1f794fe7871c7df0016ac92c9a1c01
m1arsen <m1arsen.web@gmail.com>
2023-03-03 19:30:21 +0300

    add prop-types

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -131,6 +133,10 @@ const View = ({char}) => {
       </ul>
     </>
   )
+}
+
+CharInfo.propTypes = {
+  charId: PropTypes.number
 }
 
 export default CharInfo;
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -108,6 +110,10 @@ class CharList extends Component{
       </div>
     )
   }
+}
+
+CharList.protoTypes = {
+  onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
