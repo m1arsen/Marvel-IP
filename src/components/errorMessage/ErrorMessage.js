57dd727a194d0a9277233d0c64bb800fd457bb95
m1arsen <m1arsen.web@gmail.com>
2023-05-26 21:27:52 +0300

    update errorMessage

--- src/components/errorMessage/ErrorMessage.js
+++ src/components/errorMessage/ErrorMessage.js
@@ -1,8 +1,6 @@
-import img from './error.gif';
-
 const ErrorMessage = () => {
   return(
-    <img src={img} alt='Error' style={{display: 'block', width: '250px', height: '250px', objectFit: 'contain', margin: '0 auto'}}/>
+    <h2 style={{textAlign: 'center', color: '#fff'}}>Error</h2>
   )
 }
 
