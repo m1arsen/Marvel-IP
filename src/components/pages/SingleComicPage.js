f126ec92786a4e2c79c5820788f5c13c7f3a0744
m1arsen <m1arsen.web@gmail.com>
2023-05-26 19:54:59 +0300

    bug fix - SingleComicPage

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -1,33 +1,34 @@
-import { useParams, Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-import useMarvelService from '../../services/MarvelService';
-import Spinner from '../spinner/Spinner';
-import ErrorMessage from '../errorMessage/ErrorMessage';
-import './singleComicPage.scss';
+import useMarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import "./singleComicPage.scss";
 
 const SingleComicPage = () => {
-  const {comicId} = useParams();
+  const { comicId } = useParams();
   const [comic, setComic] = useState(null);
-  const {loading, error, getComic, clearError} =  useMarvelService();
+  const { loading, error, getComic, clearError } = useMarvelService();
 
   useEffect(() => {
     updateComic();
-  }, [comicId])
+  }, [comicId]);
 
   const updateComic = () => {
     clearError();
-    getComic(comicId)
-      .then(onComicLoaded)
-  }
+    getComic(comicId).then(onComicLoaded);
+  };
 
   const onComicLoaded = (comic) => {
     setComic(comic);
-  }
+  };
 
-  const errorMessage = error ? <ErrorMessage/> : null;
-  const spinner = loading ? <Spinner/> : null;
-  const content = !(loading || errorMessage || !comic) ? <View comic={comic}/> : null;
+  const errorMessage = error ? <ErrorMessage /> : null;
+  const spinner = loading ? <Spinner /> : null;
+  const content = !(loading || errorMessage || !comic) ? (
+    <View comic={comic} />
+  ) : null;
 
   return (
     <>
@@ -35,15 +36,16 @@ const SingleComicPage = () => {
       {spinner}
       {content}
     </>
-  )
-}
+  );
+};
 
-const View = ({comic}) => {
-  const {title, description, pageCount, thumbnail, language, price} = comic;
+const View = ({ comic }) => {
+  const { title, description, pageCount, thumbnail, language, price } = comic;
+  const navigate = useNavigate();
 
   return (
     <div className="single-comic">
-      <img src={thumbnail} alt={title} className="single-comic__img"/>
+      <img src={thumbnail} alt={title} className="single-comic__img" />
       <div className="single-comic__info">
         <h2 className="single-comic__name">{title}</h2>
         <p className="single-comic__descr">{description}</p>
@@ -51,9 +53,11 @@ const View = ({comic}) => {
         <p className="single-comic__descr">Language: {language}</p>
         <div className="single-comic__price">{price}</div>
       </div>
-      <Link to={'/Marvel-IP/comics'} className="single-comic__back">Back to all</Link>
+      <Link to={navigate(-1)} className="single-comic__back">
+        Back to all
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default SingleComicPage;
