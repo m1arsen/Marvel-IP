7faf9fa162c543bcdc0fa5f3dc528fd9a16d4e4c
m1arsen <m1arsen.web@gmail.com>
2023-06-07 23:02:56 +0300

    Updates

--- static/js/952.920fefdb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmarvel_ip=self.webpackChunkmarvel_ip||[]).push([[952],{952:function(e,n,s){s.r(n),s.d(n,{default:function(){return c}});var a=s(87),t=s(519),r=s(184),c=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)(t.Z,{}),(0,r.jsx)("p",{className:"notFound__message",children:"doesn't exist"}),(0,r.jsx)(a.rU,{to:"/Marvel-IP",className:"notFound__backBtn",children:"Back to main page"})]})}}}]);
-//# sourceMappingURL=952.920fefdb.chunk.js.map
